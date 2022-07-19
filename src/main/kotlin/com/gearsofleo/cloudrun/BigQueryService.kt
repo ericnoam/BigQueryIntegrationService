f3ffc63e46e816d3f927829bef1fd1d1c3c89701
ericnoam <eric.rodriguez@leovegas.com>
2022-07-20 01:44:34 +0200

    Use Result

--- src/main/kotlin/com/gearsofleo/cloudrun/BigQueryService.kt
+++ src/main/kotlin/com/gearsofleo/cloudrun/BigQueryService.kt
@@ -4,6 +4,7 @@ import com.google.cloud.bigquery.BigQueryException
 import com.google.cloud.bigquery.BigQueryOptions
 import com.google.cloud.bigquery.QueryJobConfiguration
 import com.google.cloud.bigquery.TableId
+import com.google.cloud.bigquery.TableResult
 
 
 object BigQueryService {
@@ -17,52 +18,29 @@ object BigQueryService {
         tableName: String,
         destinationDataset: String,
         destinationTableId: String
-    ): Boolean {
-        try {
-            val query = "SELECT * FROM $projectId.$datasetName.$tableName"
+    ): Result<TableResult> {
+        val query = "SELECT * FROM $projectId.$datasetName.$tableName"
 
-            // Identify the destination table
-            val destinationTable = TableId.of(destinationDataset, destinationTableId)
+        // Identify the destination table
+        val destinationTable = TableId.of(destinationDataset, destinationTableId)
 
-            // Build the query job
-            val queryConfig = QueryJobConfiguration.newBuilder(query).setDestinationTable(destinationTable).build()
+        // Build the query jobß
+        val queryConfig = QueryJobConfiguration.newBuilder(query).setDestinationTable(destinationTable).build()
 
-            // Execute the query.
-            kotlin.runCatching {  bigquery.query(queryConfig) }
-
-            // The results are now saved in the destination table.
-            return true
-        } catch (e: BigQueryException) {
-            println("Saved query did not run \n$e")
-        } catch (e: InterruptedException) {
-            println("Saved query did not run \n$e")
-        }
-
-        return false
+        // Execute the query.
+        return kotlin.runCatching { bigquery.query(queryConfig) }
     }
 
     fun runQuery(
         projectId: String,
         datasetName: String,
         tableName: String
-    ): Boolean {
-        try {
+    ): Result<TableResult> {
+
             //unified-welder-349221.jaffle_shop.customers
             val query = "SELECT * FROM $projectId.$datasetName.$tableName"
             // For more information on Job see:
             // https://googleapis.dev/java/google-cloud-clients/latest/index.html?com/google/cloud/bigquery/package-summary.html
-            val results = bigquery.query(QueryJobConfiguration.of(query))
-            results
-                .iterateAll()
-                .forEach { row -> row.forEach { `val` -> System.out.printf("%s\n", `val`.toString()) } }
-        } catch (e: BigQueryException) {
-            println("Table extraction job was interrupted. \n$e")
-            return false
-        } catch (e: InterruptedException) {
-            println("Table extraction job was interrupted. \n$e")
-            return false
-        }
-
-        return true
+            return kotlin.runCatching { bigquery.query(QueryJobConfiguration.of(query)) }
     }
 }
--- src/main/kotlin/com/gearsofleo/cloudrun/Main.kt
+++ src/main/kotlin/com/gearsofleo/cloudrun/Main.kt
@@ -4,17 +4,29 @@ fun main(args: Array<String>) {
     // These values are provided automatically by the Cloud Run Jobs runtime.
     val CLOUD_RUN_TASK_INDEX = System.getenv().getOrDefault("CLOUD_RUN_TASK_INDEX", "0")
     val CLOUD_RUN_TASK_ATTEMPT = System.getenv().getOrDefault("CLOUD_RUN_TASK_ATTEMPT", "0")
-    val projectId = "unified-welder-349221"
+    val projectId = System.getenv("GOOGLE_CLOUD_PROJECT")
     val datasetName = "jaffle_shop"
     val tableName = "customers"
     val destinationDataset = "dataform"
-    //val success = BigQueryService.runQuery(projectId, datasetName, tableName)
-    val success = BigQueryService.saveQueryToTable (projectId, datasetName, tableName, destinationDataset, tableName)
-    if (success) {
-        println("Query run successfully.")
-    } else {
-        System.err.println( "Task #$CLOUD_RUN_TASK_INDEX, Attempt #$CLOUD_RUN_TASK_ATTEMPT failed.")
-    }
+    val result = BigQueryService.runQuery(projectId, datasetName, tableName)
+
+    result.fold(
+        { it.iterateAll()
+            .forEach { row -> row.forEach { `val` -> System.out.printf("%s\n", `val`.toString()) } }
+            it.
+        },
+        { System.err.println(it.localizedMessage) }
+    )
+
+    /*val result = BigQueryService.saveQueryToTable(projectId, datasetName, tableName, destinationDataset, tableName)
+    result
+        .fold(
+            { println("Query run successfully for ${it.totalRows} rows") },
+            {
+                System.err.println("Task #$CLOUD_RUN_TASK_INDEX, Attempt #$CLOUD_RUN_TASK_ATTEMPT failed.")
+                System.err.println(it.localizedMessage)
+            }
+        )*/
 }
 
 
